c6c8347000da9e7bb9088138b0c16c247d1a7ae9
MkVerma643 <mukeshverma643@gmail.com>
2021-07-09 11:47:41 +0530

    making changes

--- index.js
+++ index.js
@@ -9,5 +9,6 @@ app.get("/", (req, res) => {
 });
 
 const PORT = process.env.PORT || 5000;
+//const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
